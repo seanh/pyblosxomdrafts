a66450c12c7d78f6671d42579595d1a38a06c50a
Sean Hammond <git@seanh.cc>
2015-09-28 09:38:12 +0100

    Use cb_truncatelist()
    cb_prepare() is not the right place to delete entries from the entry
    list, doesn't work as desired.

--- pyblosxomdrafts/__init__.py
+++ pyblosxomdrafts/__init__.py
@@ -13,13 +13,14 @@ def verify_installation(request):
     return True
 
 
-def cb_prepare(args):
+def cb_truncatelist(args):
     data = args['request'].get_data()
+
     # Do show drafts on their own permalink pages.
     if data['bl_type'] == 'file':
         return
 
     # Filter drafts out on non-permalink pages.
-    data['entry_list'] = [
-        e for e in data['entry_list']
+    return [
+        e for e in args['entry_list']
         if 'drafts' not in e.get_metadata('absolute_path').split(os.path.sep)]
